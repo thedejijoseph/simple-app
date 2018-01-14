e380984bbc5f939f487d9365da5be25c3552e8b1
Deji Joseph <wrecodde@gmail.com>
2018-01-14 15:03:50 +0100

    added port definition

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tornado.web
 import tornado.httpserver
 import tornado.ioloop
+import tornado.options
+
+from tornado.options import define
+define("port", default=5000, help="open at given port", type=int)
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
@@ -19,8 +23,9 @@ app = tornado.web.Application(
 )
 
 def start():
+	tornado.options.parse_command_line()
 	server = tornado.httpserver.HTTPServer(app)
-	server.listen(5000)
+	server.listen(tornado.options.options.port)
 	
 	tornado.ioloop.IOLoop.instance().start()
 
