751640ac05fff27020866368b6d111b95d793e54
Deji Joseph <wrecodde@gmail.com>
2018-01-29 17:18:56 +0100

    added port def to Procfile

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 
+import logging
+
+logging.basicConfig(
+level = logging.DEBUG,
+format = "%(asctime)s | %(message)s")
+
 from tornado.options import define
 define("port", default=5000, help="open at given port", type=int)
+define("cup", default="coffe")
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
@@ -26,6 +33,9 @@ def start():
 	tornado.options.parse_command_line()
 	server = tornado.httpserver.HTTPServer(app)
 	server.listen(tornado.options.options.port)
+	
+	print(tornado.options.options.items())
+	print(tornado.options.options.cup)
 	
 	tornado.ioloop.IOLoop.instance().start()
 
